00f5021e24703705d8f8298b4b94f2ac757fce0a
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-02-18 14:35:21 +0000

    Stupid coach project done with 3 tests

---
+++ test/system/questions_test.rb
@@ -0,0 +1,27 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "h3", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+    take_screenshot
+  end
+
+  test "asking a question" do
+    visit ask_url
+    fill_in "question", with: "how are you?"
+    click_on "Ask"
+
+    take_screenshot
+    assert_text "Silly question, get dressed and go to work!"
+  end
+
+end
